04783a85ffa8c0be9abae3bce0dbef3480cc1498
dpkk <dpkk@netcompany.com>
2021-05-20 14:00:44 +0200

    Database project created

---
+++ EITParcelDeliveryDB/Tables/Parcel.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Parcel]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[Type] INT NOT NULL FOREIGN KEY REFERENCES [dbo].[ParcelTypes](Id),
+	[Price] INT NOT NULL,
+	[Weight] INT NOT NULL,
+	[ETA] DATETIME NOT NULL,
+	[Start] INT NOT NULL FOREIGN KEY REFERENCES [dbo].[PathComponents](Id),
+	[Status] INT NOT NULL FOREIGN KEY REFERENCES [dbo].[ParcelStatus](Id)
+)
---
+++ EITParcelDeliveryDB/Tables/WeightPrices.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[WeightPrices]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[Min] INT NOT NULL,
+	[Max] INT NOT NULL,
+	[Price] INT NOT NULL
+)

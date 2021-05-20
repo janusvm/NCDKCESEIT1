e250ca2c5007555280957d8a7523d63b25211bd4
dpkk <dpkk@netcompany.com>
2021-05-21 01:33:44 +0200

    Minor

---
+++ EITParcelDelivery/Repositories/CitiesRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace EITParcelDelivery.Repositories
+{
+    public class CitiesRepository
+    {
+        public List<int> getAll()
+        {
+            
+        }
+    }
+}

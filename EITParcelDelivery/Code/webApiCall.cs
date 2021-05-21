d2fb51534e441381134d63c7b18ba0953ccb9718
U-NCLAN\rtcp <rtcp@PF1CK5K1.nclan.netcompany.dk>
2021-05-21 02:37:52 +0200

    wip

---
+++ EITParcelDelivery/Code/webApiCall.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Net;
+using System.IO;
+
+namespace EITParcelDelivery.Code
+{
+    public class webApiCall
+    {
+        public string getParcel()
+        {
+            WebClient client = new WebClient();
+            Stream data = client.OpenRead("http://wa-eit-dk1.azurewebsites.net/api/parcelinformation?source=test&destination=test&type=1&token=hej");
+            StreamReader reader = new StreamReader(data);
+            string s = reader.ReadToEnd();
+            data.Close();
+            reader.Close();
+            return s;
+        }
+    }
+}

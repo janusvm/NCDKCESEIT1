405851b6944bcd9eca881772b446528c2662219c
dpkk <dpkk@netcompany.com>
2021-05-21 04:49:22 +0200

    Minor

--- EITParcelDelivery/Controllers/HomeController.cs
+++ EITParcelDelivery/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EITParcelDelivery.Models;
 using EITParcelDelivery.Repositories;
 
 namespace EITParcelDelivery.Controllers

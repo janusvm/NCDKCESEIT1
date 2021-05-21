93205c1ca8911544f52416dffd92fa621ed5d5cc
Janus S. Valberg-Madsen <jsvm@netcompany.com>
2021-05-21 04:25:16 +0200

    Work on views

---
+++ EITParcelDelivery/Controllers/SendParcelController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EITParcelDelivery.Models;
+
+namespace EITParcelDelivery.Controllers
+{
+    public class SendParcelController : Controller
+    {
+        // GET: SendParcel
+        public ActionResult Index()
+        {
+            var x = new SendParcelViewModel();
+            x.Cities = new List<City>
+            {
+                new City { Id = 1, name = "Teehee", Available = true },
+                new City { Id = 2, name = "Poopee", Available = false }
+            };
+            return View(x);
+        }
+    }
+}
---
+++ EITParcelDelivery/Models/SendParcelViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace EITParcelDelivery.Models
+{
+    public class SendParcelViewModel
+    {
+        [Required]
+        [Range(0, 100)]
+        [DisplayName("Weight (in kg)")]
+        public int Weight { get; set; }
+
+        [Required]
+        public City Source { get; set; }
+
+        [Required]
+        public City Destination { get; set; }
+
+        [Required]
+        public ParcelType Type { get; set; }
+
+        public List<City> Cities { get; set; }
+
+        public IEnumerable<string> getCityNames()
+        {
+            return Cities.Where(c => c.Available).Select(c => c.name);
+        }
+    }
+}
